da3aabcfe5bd12520fe2d037740f25ab0c54c39c
Leandro Stipanich <leandro@ironsystems.com.br>
2020-04-19 18:00:20 -0300

    Desafio finalizado

--- src/App.js
+++ src/App.js
@@ -1,30 +1,91 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      // console.log(response)
+      setRepositories(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      "title": `Novo repositório ${Date.now()}`,
+      "url": `https://github.com/lestipa/new${Date.now()}`,
+      "techs": []
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
-  }
+    await api.delete(`repositories/${id}`).then( response => {
+      const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+      if ( repositoryIndex >= 0 ) {
+        const test = [{
+          "title": `Novo repositório ${Date.now()}`,
+          "url": `https://github.com/lestipa/new${Date.now()}`,
+          "techs": []
+        }];
+        
+        const data = [];
+        repositories.map( repository => data.push(repository.id !== id ? repository : []) );
+        console.log()
+        setRepositories(data);
+        // console.log('Removido index: ', repositoryIndex)
+        // console.log('data: ', data)
+        // console.log('repositories: ', repositories)
+      }
+    });
+
+    // await api.delete(`repositories/${id}`, () => {
+    //   const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    //   if ( repositoryIndex >= 0 ) {
+    //     const data = repositories;
+    //     data.splice(repositoryIndex, 1);
+    //     setRepositories(data);
+    //   }
+    // })
+  };
 
   return (
-    <div>
+    <>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => 
+          <>
+          {
+            repository.id &&
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          }
+          </>
+        )}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {/* {
+          repositories.map(repository => (
+              <li key={repository.id}>
+                {repository.title}
+                <button onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+                </button>
+              </li>
+          ))
+        } */}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+    </>
   );
 }
 
